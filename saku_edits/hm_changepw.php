f4a9237f13f0cf1838a9dc0b7c7b71b7f974bf2e
“SakethaYellanki“ <sakethayellanki05@gmail.com>
2020-05-16 13:01:06 +0530

    modified change password UI

---
+++ saku_edits/hm_changepw.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Change Password</title>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" />
+	<link rel="stylesheet" type="text/css" href="change_pw.css">
+</head>
+<body>
+<div class="container" id="container">
+	<div class="form-container">
+		<h2>Change Password</h2>
+		<form action="#">
+			
+			<input type="Password" name="#" placeholder="Previous Password" />
+			<input type="Password" name="#" placeholder="New Password" />
+			<input type="Password" name="#" placeholder="Confirm New Password" />
+			<button type="submit">Save</button>
+			
+		</form>
+		
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel">
+				<p>Yup Mail</p>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+
+</body>
+</html>
